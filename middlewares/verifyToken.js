91d64bc4d66646a914c3b1dec55315e940bd70e9
PhamPhong137 <pp072003@gmail.com>
2025-03-09 00:20:32 +0700

    fix: update verifyToken

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -3,18 +3,26 @@ const httpErrors = require('http-errors');
 
 const verifyToken = async (req, res, next) => {
     try {
-        const receiveToken = req.headers["x-access-token"] || req.headers["authorization"]?.split(" ")[1];
+        const token = req.headers['authorization'];
 
-        if (!receiveToken) {
-            throw httpErrors.BadRequest("No token provide");
-        }
+        if (!token) return res.status(403).json({
+            message: "No token provided!",
+            status: 403
+        });
 
-        jwt.verify(receiveToken, process.env.JWT_SECRET, (err, decoded) => {
+        const bearer = token.split(' ');
+        const bearerToken = bearer[1];
+
+        jwt.verify(bearerToken, process.env.JWT_SECRET, (err, decoded) => {
             if (err) {
-                return res.status(401).send({ message: "Unauthorized!" });
+                return res.status(401).send({
+                    message: "Unauthorized!",
+                    status: 401
+                });
             }
             req.userId = decoded.userId;
             req.role = decoded.role;
+            
             next();
         })
     } catch (error) {
